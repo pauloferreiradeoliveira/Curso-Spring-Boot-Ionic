7fbed33372ca1f8397a946398bc5d47dceb7b122
Paulo Ferreira <mrpauloii@gmail.com>
2018-07-23 11:08:53 -0300

    Mostando dados e imagem do cliente para pagina de profile

--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { StorageService } from '../../services/storage.service';
+import { ClienteDTO } from '../../models/cliente.dto';
+import { ClienteService } from '../../services/cliente.service';
+import { API_CONFIG } from '../../config/api.config';
 
 
 @IonicPage()
@@ -10,20 +13,37 @@ import { StorageService } from '../../services/storage.service';
 })
 export class ProfilePage {
 
-  email: string;
+  cliente: ClienteDTO;
 
   constructor(
     public navCtrl: NavController,
     public navParams: NavParams,
-    public storage: StorageService) {
+    public storage: StorageService,
+    public clienteService: ClienteService) {
 
   }
 
   ionViewDidLoad() {
     let localuser = this.storage.getLocalUser();
+
     if(localuser && localuser.email){
-      this.email = localuser.email;
+      this.clienteService.findByEmail(localuser.email).subscribe(
+        response => {
+          this.cliente = response;
+          this.getImageIfExists();
+        },
+        error => {});
     }
+  }
+
+  getImageIfExists(){
+    this.clienteService.getImageFromBuket(this.cliente.id)
+      .subscribe(
+        response => {
+          this.cliente.imageURL = `${API_CONFIG.bucketBaseUrl}cp${this.cliente.id}.jpg`
+        },
+        error => {}
+      )
   }
 
 }
